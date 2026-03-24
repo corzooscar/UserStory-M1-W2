20fb8da677300f6b7afd51c67b6260607b700151
Oscar Corzo <corzoosacr143@gmail.com>
2026-03-24 01:35:40 -0500

    Commit: Implemented loop for multiple records

--- main.py
+++ main.py
@@ -15,35 +15,41 @@ MAGENTA = "\033[35m"
 CYAN    = "\033[36m"
 WHITE   = "\033[37m"
 
+Inventory = []
 Option = None
-try:    
-    Option = int(input(f"""{BOLD}{MAGENTA}{"="*24}{RESET}{BOLD}{GREEN} MAIN MENU{RESET} {BOLD}{MAGENTA}{"="*25}  {RESET}
-{BOLD}{MAGENTA}1){RESET}{GREEN} 🔍 Add product{RESET}
-{BOLD}{MAGENTA}2){RESET}{GREEN} 💳 Show inventory{RESET}
-{BOLD}{MAGENTA}3){RESET}{GREEN} 💰 Calculate statistics{RESET}
-{BOLD}{MAGENTA}4){RESET}{GREEN} ✅ Exit{RESET}
-{BOLD}{MAGENTA}{"="*60}{RESET}
-{BOLD}{GREEN}Choose an option:{RESET}
-{MAGENTA}➤  {RESET}""")) 
-    if Option not in [1,2,3,4]:
-        print(f"{BOLD}{RED}{"❌ ERROR: Please enter a number between 1 and 4. ❌".center(60)}{RESET}")
-        Option = int(input(f"""{BOLD}{MAGENTA}{"="*24}{RESET}{BOLD}{GREEN} MAIN MENU{RESET} {BOLD}{MAGENTA}{"="*25}  {RESET}
-{BOLD}{MAGENTA}1){RESET}{GREEN} 🔍 Add product{RESET}
-{BOLD}{MAGENTA}2){RESET}{GREEN} 💳 Show inventory{RESET}
-{BOLD}{MAGENTA}3){RESET}{GREEN} 💰 Calculate statistics{RESET}
-{BOLD}{MAGENTA}4){RESET}{GREEN} ✅ Exit{RESET}
+
+while 777:
+    try:    
+        Option = int(input(f"""{BOLD}{MAGENTA}{"="*24}{RESET}{BOLD}{GREEN} MAIN MENU{RESET} {BOLD}{MAGENTA}{"="*25}{RESET}
+{BOLD}{MAGENTA}1){RESET}{GREEN} 🩲​ Add product{RESET}
+{BOLD}{MAGENTA}2){RESET}{GREEN} 📦​ Show inventory{RESET}
+{BOLD}{MAGENTA}3){RESET}{GREEN} 🧾​ Calculate statistics{RESET}
+{BOLD}{MAGENTA}4){RESET}{GREEN} 🔚​ Exit{RESET}
 {BOLD}{MAGENTA}{"="*60}{RESET}
 {BOLD}{GREEN}Choose an option:{RESET}
 {MAGENTA}➤  {RESET}"""))
-except ValueError:
-    print(f"{BOLD}{RED}{"❌ ERROR: Please enter a valid input. ❌".center(60)}{RESET}")
+        if Option == 1:
+                Process = None
+                while Process != "exit":
+                    print(f"{BOLD}{MAGENTA}{"="*23}{RESET}{BOLD}{GREEN} NEW PRODUCT{RESET} {BOLD}{MAGENTA}{"="*24}{RESET}")
+                    product_name = input(f"{GREEN}~ Product name: {RESET}")
+                    unit_price   = float(input(f"{GREEN}~ Unit price: ${RESET}"))
+                    quantity     = int(input(f"{GREEN}~ Quantity: {RESET}"))
+                    subtotal     = unit_price * quantity
+                    Product = {"Product": product_name, "Price": unit_price, "Quantity": quantity}
+                    Inventory.append(Product)
+                    print(f"{GREEN}{"Product added successfully!".center(60)}{RESET}\n")
+                    Process = input(f"{YELLOW}{ITALIC}Type 'exit' to return to the main menu, or press Enter to\nadd another product: {RESET}").lower()
 
-if Option == 1:
-    print("NOT IMPLEMENTED YET")
-elif Option == 2:
-    print("NOT IMPLEMENTED YET")
-elif Option == 3:
-    print("NOT IMPLEMENTED YET")
-elif Option == 4:
-    print("Exiting the program. Goodbye!")
-    exit()
+        elif Option == 2:
+            print("NOT IMPLEMENTED YET")
+        elif Option == 3:
+            print("NOT IMPLEMENTED YET")
+        elif Option == 4:
+            print("Exiting the program. Goodbye!")
+            exit()
+        else:
+            print(f"{BOLD}{RED}{"❌ ERROR: Please enter a number between 1 and 4. ❌".center(60)}{RESET}")
+    except ValueError:
+        print(f"{BOLD}{RED}{"❌ ERROR: Please enter a valid input. ❌".center(60)}{RESET}")
+
